c926cca6eead5192e2e98648de700f23d3169eca
saca1026 <70294134+saca1026@users.noreply.github.com>
2021-12-13 20:23:34 -0700

    Add files via upload

--- Controllers/ned_arm2_controller.py
+++ Controllers/ned_arm2_controller.py
@@ -5,13 +5,6 @@ robot = Robot()
 
 timestep = int(robot.getBasicTimeStep())
 
-# def sleep(duration):
-    # global robot
-    # endtime = robot.getTime() + duration
-    
-    # while robot.step(timestep) != -1 and robot.getTime() < duration: # end_time?
-        # pass
-
 def sleep(duration):
     global robot
     end_time = robot.getTime() + duration
@@ -28,13 +21,14 @@ m6 = robot.getDevice('joint_6')
 m7 = robot.getDevice('joint_base_to_jaw_1')
 m8 = robot.getDevice('joint_base_to_jaw_2')
 
-sleep(20)
+# nothing until e-puck gets around
+sleep(90)
 
-m1.setPosition(-1.6)
-m2.setPosition(0.5)
-m3.setPosition(0.4)
+m1.setPosition(0)
+m2.setPosition(0.65)
+m3.setPosition(0.1)
 m4.setPosition(0)
-m5.setPosition(-0.3)
+m5.setPosition(-0.8)
 m6.setPosition(0)
 m7.setPosition(0.01)
 m8.setPosition(0.01)
@@ -48,30 +42,39 @@ m6.setVelocity(1)
 m7.setVelocity(1)
 m8.setVelocity(1)
 
-#print("before sleep")
 sleep(2)
-#time.sleep(2)
 
-#print("after sleep")
-m2.setPosition(0.72)
-m3.setPosition(0.5)
-m5.setPosition(-0.4)
-
-m2.setVelocity(1)
-m3.setVelocity(1)
-m5.setVelocity(1)
-
-sleep(2)
-# #time.sleep(2)
-
-m7.setPosition(0) # robot's left prong
-m8.setPosition(0) # robot's right (my left)
+m7.setPosition(0)
+m8.setPosition(0)
 
 m7.setVelocity(1)
 m8.setVelocity(1)
 
 sleep(2)
-# #time.sleep(2)
+
+m1.setPosition(1.6)
+m2.setPosition(0.4)
+m3.setPosition(0.9)
+m4.setPosition(0)
+m5.setPosition(-0.3)
+m6.setPosition(0)
+
+m1.setVelocity(1)
+m2.setVelocity(1)
+m3.setVelocity(1)
+m4.setVelocity(1)
+m5.setVelocity(1)
+m6.setVelocity(1)
+
+sleep(2) 
+
+m7.setPosition(0.01)
+m8.setPosition(0.01)
+
+m7.setVelocity(1)
+m8.setVelocity(1)
+
+sleep(2)
 
 m1.setPosition(0)
 m2.setPosition(0)
@@ -82,25 +85,11 @@ m6.setPosition(0)
 m7.setPosition(0)
 m8.setPosition(0)
 
-m1.setVelocity(0.5)
-m2.setVelocity(0.5)
-m3.setVelocity(0.5)
-m4.setVelocity(0.5)
-m5.setVelocity(0.5)
-m6.setVelocity(0.5)
-m7.setVelocity(0.5)
-m8.setVelocity(0.5)
-
-# position of e-puck stopped
-m1.setPosition(0)
-m2.setPosition(0.65)
-m3.setPosition(0.1)
-m4.setPosition(0)
-m5.setPosition(-0.8)
-m6.setPosition(0)
-
-sleep(2)
-
-m7.setPosition(0.01)
-m8.setPosition(0.01)
-
+m1.setVelocity(1)
+m2.setVelocity(1)
+m3.setVelocity(1)
+m4.setVelocity(1)
+m5.setVelocity(1)
+m6.setVelocity(1)
+m7.setVelocity(1)
+m8.setVelocity(1)
